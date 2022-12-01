ec63495af6e61f1b4400c37de8a4bfb8a6bc9ada
Lim-Dolphin <65551867+Lim-Dolphin@users.noreply.github.com>
2022-12-02 01:08:11 +0900

    3.1-4

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    //이동 속도
+    public float speed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = Vector3.up;
+
+        transform.position += dir * speed * Time.deltaTime;
+    }
+}

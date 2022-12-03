283162758549d97e378612e0c972aaa363831adf
Lim-Dolphin <65551867+Lim-Dolphin@users.noreply.github.com>
2022-12-03 10:26:14 +0900

    3.1

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,18 +4,35 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    Vector3 dir;
+
     public float speed = 5;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        int ranValue = UnityEngine.Random.Range(0, 10);
+
+        if (ranValue < 3)
+        {
+            GameObject target = GameObject.Find("Player");
+
+            dir = target.transform.position - transform.position;
+
+            //방향크기 1
+            dir.Normalize();
+        }
+        else
+        {
+            dir = Vector3.down;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 dir = Vector3.down;
+        //Vector3 dir = Vector3.down;
 
         transform.position += dir * speed * Time.deltaTime;
     }
